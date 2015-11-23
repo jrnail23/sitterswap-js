a580c441a4e8bf1c5c06faeedf924e5c4e5cd94b
James Nail <james.nail@lanetix.com>
2015-11-23 14:17:08 -0500

    introduced lanetix/make-store

--- src/stores/membersStore.js
+++ src/stores/membersStore.js
@@ -1,43 +1,23 @@
 import Dispatcher from '../dispatcher'
+import makeStore from '@lanetix/make-store'
 import ActionTypes from '../constants/actionTypes'
-import {EventEmitter} from 'events'
 import _ from 'lodash'
-
-var CHANGE_EVENT = 'change'
 
 var _members = []
 
-var MembersStore = _.extend({
-  addChangeListener: function (callback) {
-    this.on(CHANGE_EVENT, callback)
-  },
-  removeChangeListener: function (callback) {
-    this.removeListener(CHANGE_EVENT, callback)
-  },
-  emitChange: function () {
-    this.emit(CHANGE_EVENT)
-  },
-  getAllMembers: function () {
+let methods = {
+  getAllMembers () {
     return _members
   },
-  getMemberByKey: function (key) {
+
+  getMemberByKey (key) {
     return _.find(_members, {key: key})
   }
-}, EventEmitter.prototype)
+}
 
-MembersStore.dispatcher = Dispatcher.register(function (action) {
-  switch (action.actionType) {
-    case ActionTypes.INITIALIZE:
-      _members = action.initialData.members
-      MembersStore.emitChange()
-      break
-    case ActionTypes.ADD_MEMBER:
-      _members.push(action.member)
-      MembersStore.emitChange()
-      break
-    default:
-      // no-op
-  }
-})
+let handlers = {
+  [ActionTypes.INITIALIZE]: action => { _members = action.initialData.members },
+  [ActionTypes.ADD_MEMBER]: action => { _members.push(action.member) }
+}
 
-export default MembersStore
+export default makeStore(methods, handlers, Dispatcher)
