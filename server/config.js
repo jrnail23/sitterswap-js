34c2ca962266d8aefe05f3baaa3d55baef68425e
James Nail <james.nail@lanetix.com>
2015-11-29 01:55:59 -0500

    changed db username for app

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 export default {
-  databaseUrl: process.env.DATABASE_URL || 'postgres://jrnail23@localhost/sitterswap',
+  databaseUrl: process.env.DATABASE_URL || 'postgres://postgres@localhost/sitterswap',
   port: process.env.PORT || 9006,
   corsOptions: {
     allowedOrigins: ['localhost:*'],
