2483807dcc854e3daae189c6ebd82877466f32d1
James Nail <james.nail@lanetix.com>
2015-12-23 12:34:51 -0500

    remove api stuff

--- src/server/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  databaseUrl: process.env.DATABASE_URL || 'postgres://postgres@localhost/sitterswap',
-  port: process.env.PORT || 9006,
-  corsOptions: {
-    allowedOrigins: ['localhost:*'],
-    headers: ['Content-Type', 'X-Requested-With', 'Location', 'ETag']
-  }
-}
